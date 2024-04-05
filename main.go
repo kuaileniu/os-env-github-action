75c08e41e301e28d95c31837f1fbb02324647bff
unknown <hggfjt@126.com>
2024-04-05 15:43:50 +0800

    f

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import "os"
 
 func main() {
 	pwd := os.Getenv("pwd")
-
-	println("pwd:" + pwd[0:3] + " - " + pwd[3:])
-	println("pwd:" + pwd[0:3] + "" + pwd[3:])
+	println("pwd:" + pwd[0:3] + " - " + pwd[3:]) // 因 凡是与 secrets.PWD 相同的均被显示为*，为了显示出来，所以中间才加个-符号
+	println("pwd:" + pwd)
 	println("输出完毕")
 }
