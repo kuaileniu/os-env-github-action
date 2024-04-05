f5e1a30ede57f0aaf2d16801c35adcfc9a82d383
unknown <hggfjt@126.com>
2024-04-05 15:50:48 +0800

    添加备注

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "os"
 
 func main() {
-	pwd := os.Getenv("pwd")
+	pwd := os.Getenv("pwd")  // 测试密码github action中配置为 abcd中文！@!@#$%^&*
 	println("pwd:" + pwd[0:3] + " - " + pwd[3:]) // 因 凡是与 secrets.PWD 相同的均被显示为*，为了显示出来，所以中间才加个-符号
 	println("pwd:" + pwd)
 	println("输出完毕")
